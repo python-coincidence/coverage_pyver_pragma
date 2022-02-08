f7924eb17a1ee59364ddb1d62ce8d88e324fe6bb
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-02-08 21:10:09 +0000

    Test on Python 3.11 and PyPy 3.8

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -15,7 +15,7 @@ platforms.remove(platform.system())
 platforms = {*platforms, *(p.lower() for p in platforms)}
 
 versions = [(2, 7)]
-versions.extend((3, v) for v in range(11))
+versions.extend((3, v) for v in range(12))
 versions.append((4, 0))
 
 versions_before = versions[:versions.index(sys.version_info[:2])]
