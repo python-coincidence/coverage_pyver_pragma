68dc5a0ad672f6711aed3da95bee72b08c2504ba
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-12-24 11:05:11 +0000

    Bump version v0.3.3 -> v0.4.0

--- coverage_pyver_pragma/__init__.py
+++ coverage_pyver_pragma/__init__.py
@@ -44,7 +44,7 @@ from coverage_pyver_pragma.grammar import GRAMMAR
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020-2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.3.3"
+__version__: str = "0.4.0"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = ("DSL_EXCLUDE", "evaluate_exclude")
--- doc-source/index.rst
+++ doc-source/index.rst
@@ -107,7 +107,7 @@ coverage_pyver_pragma
 		:alt: GitHub top language
 
 	.. |commits-since| github-shield::
-		:commits-since: v0.3.3
+		:commits-since: v0.4.0
 		:alt: GitHub commits since tagged version
 
 	.. |commits-latest| github-shield::
